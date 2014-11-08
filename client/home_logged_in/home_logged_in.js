0f50f1299b306c764c3d58402774a69dfa65973d
Jarno Le Conté <jarno.leconte@me.com>
2014-11-08 22:56:42 +0100

    Live updating graph

--- client/home_logged_in/home_logged_in.js
+++ client/home_logged_in/home_logged_in.js
@@ -7,17 +7,22 @@ Template.home_logged_in.rendered = function() {
 
   var resize = function() {
     canvas.width = $(window).width();
-    canvas.height = $(window).height() * 0.3;
+    canvas.height = $(window).height() * 0.8;
     $canvas.css({width: canvas.width, height: canvas.height});
     makeChart(); 
   }
 
   var makeChart = function() {
-    myLineChart = new Chart(ctx).Line(data(), {
+    var d = data();
+    myLineChart = new Chart(ctx).Line(d, {
       showScale: false,
       showTooltips: false,
     });
   }
+
+  this.autorun(function(d) {
+    makeChart();
+  });
 
   $(window).resize(resize);
   resize();
@@ -25,24 +30,41 @@ Template.home_logged_in.rendered = function() {
 }
 
 var data = function() {
+  var data = property(Energy.findOne({historicaldata: true}), 'content.data');
+  chartData = _.last(data || [], 8000); // last day usage per 10 seconds
+  chartData = _.map(chartData, function(d) { return parseInt(d.value); });
+  
+  // take average of every 180 entries
+  var result = [];
+  var avg = [];
+  chartData = _.each(chartData, function(e) {
+    avg.push(e);
+    if (avg.length >= 180) {
+      result.push(_.reduce(avg, function(memo, num){ return memo + num; }, 0));
+      avg = [];
+    }
+  });
+  if (avg.length > 0)
+    result.push(_.reduce(avg, function(memo, num){ return memo + num; }, 0));
+
   return {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      labels: _.range(result.length),
       datasets: [
           {
               label: "My First dataset",
-              fillColor: "rgba(220,220,250,0.2)",
-              strokeColor: "rgba(220,220,250,1)",
-              pointColor: "rgba(220,220,250,1)",
+              fillColor: "rgba(150,150,255,0.2)",
+              strokeColor: "rgba(150,150,255,1)",
+              pointColor: "rgba(150,150,255,1)",
               pointStrokeColor: "#fff",
               pointHighlightFill: "#fff",
               pointHighlightStroke: "rgba(220,220,220,1)",
-              data: [65, 59, 80, 81, 56, 55, 40]
+              data: result
           },
           {
               label: "My Second dataset",
-              fillColor: "rgba(151,187,205,0.2)",
-              strokeColor: "rgba(151,187,205,1)",
-              pointColor: "rgba(151,187,205,1)",
+              fillColor: "rgba(250,250,170,0.2)",
+              strokeColor: "rgba(250,250,170,1)",
+              pointColor: "rgba(250,250,170,1)",
               pointStrokeColor: "#fff",
               pointHighlightFill: "#fff",
               pointHighlightStroke: "rgba(151,187,205,1)",
