dd9bc7aa58fb82a21b734118f3316b86cf421862
Jarno Le Conté <jarno.leconte@me.com>
2014-11-08 17:07:49 +0100

    cube dashboard

--- client/main.js
+++ client/main.js
@@ -1,1 +1,6 @@
-Session.setDefault("color", "red");
+Session.set("color", "rgb(0,250,120)");
+
+
+Template.registerHelper("hue", function() {
+  return (tinycolor(Session.get('color')).toHsl().h + 60) % 360;
+});
