ffc7d61e7f7934c31b64dd739d577e3255e36cee
Jarno Le Conté <jarno.leconte@me.com>
2014-11-09 10:03:16 +0100

    Color/effect collection

--- model.js
+++ model.js
@@ -1,4 +1,17 @@
 Energy = new Mongo.Collection("energy");
+Color = new Mongo.Collection('color')
+
+
 
 Meteor.startup(function(){
+  if (Meteor.isServer) {
+    Color.upsert({field: "color"}, {$set: {field: "color", value: "blue"}});
+    Color.upsert({field: "effect"}, {$set: {field: "effect", value: "blink"}});
+  }
 });
+
+if (Meteor.isServer) {
+  Meteor.publish('color', function() {
+    return Color.find();
+  });
+}
